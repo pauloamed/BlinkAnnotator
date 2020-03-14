c85f24f36a8f4d64d19d68fa86a67b9b4a1e1df7
pauloamed <pauloaugusto99@ufrn.edu.br>
2020-03-14 15:34:50 -0300

    adjusted log to new convention

--- visualizer.py
+++ visualizer.py
@@ -16,7 +16,7 @@ def readCSV(inputPath):
         header = lines[0].split(';')
 
         frameStartIndex = header.index('frame_start')
-        isBlinkingIndex = header.index('is_blinking')
+        isBlinkingIndex = header.index('eyes_status')
 
         lines = lines[1:]
         numEntries = len(lines)
@@ -33,16 +33,25 @@ def readCSV(inputPath):
 ap = argparse.ArgumentParser()
 ap.add_argument("-rp", "--recordsPath", required=True, help="Caminho para arquivo onde ficarão salvas as informacoes para cada frame")
 ap.add_argument("-fd", "--framesDir", required=True, help="Caminho para pasta onde ficarão salvas as imagens")
-args = vars(ap.parse_args())
+ap.add_argument("-s", "--specificFrames", required=False, help="")
+args = ap.parse_args()
 
 ######################### VIDEO CAPTURE + FACE DETECT PREP #############################
 
-numEntries, originalClasses, frameStarts = readCSV(args['recordsPath'])
+numEntries, originalClasses, frameStarts = readCSV(args.recordsPath)
+
+specificFrames = None
+if args.specificFrames:
+    specificFrames = [int(x) for x in args.specificFrames.split(',')]
 
 for i in tqdm(range(numEntries)):
-    frame = cv2.imread(os.path.join(args['framesDir'], "{}.jpg".format(i)), cv2.IMREAD_COLOR)
+    frame = cv2.imread(os.path.join(args.framesDir, "{}.jpg".format(i)), cv2.IMREAD_COLOR)
 
     cv2.putText(frame, originalClasses[i], (int(frame.shape[1] * 0.5), int(frame.shape[0] * 0.7)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+    cv2.putText(frame, str(i), (int(frame.shape[1] * 0.5), int(frame.shape[0] * 0.8)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
     cv2.imshow('Cam', frame)
+    cv2.waitKey(33)
 
-    cv2.waitKey(100)
+    if specificFrames and i in specificFrames:
+        print("Press ENTER to continue")
+        input()
