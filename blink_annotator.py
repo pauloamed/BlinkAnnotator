51be1669021e94507dc3a36d1e623330d3cd5af2
pauloamed <pauloaugusto99@ufrn.edu.br>
2020-03-17 15:08:08 -0300

    implementacao da terceira fase

--- blink_annotator.py
+++ blink_annotator.py
@@ -8,13 +8,24 @@ import random
 from tqdm import tqdm
 
 
-ORIG_DUR = 1 / 30
-TIMES_SLOWER = 3
-MOD_CUR = ORIG_DUR * TIMES_SLOWER
-FRAMES_WINDOW_SIZE = max(int(1 / MOD_CUR), 3)
+####################################################################################################
 
-print("Frame duration: " + str(MOD_CUR))
-print("Frames window size: " + str(FRAMES_WINDOW_SIZE))
+ap = argparse.ArgumentParser()
+ap.add_argument("-ip", "--inputPath", required=True, help="Caminho para arquivo CSV de entrada")
+ap.add_argument("-fd", "--framesDir", required=True, help="Caminho para pasta onde ficarão salvas as imagens")
+ap.add_argument("-op", "--outputPath", required=True, help="Caminho para arquivo CSV de saída")
+ap.add_argument("-ts", "--timesSlower", required=False, help="Quantas vezes mais lento que a execução normal", default=3)
+ap.add_argument("-t", "--threshold", required=False, help="Threshold de sequencias", default=3)
+args = vars(ap.parse_args())
+
+####################################################################################################
+
+ORIG_DUR = 1 / 30
+TIMES_SLOWER = int(args['timesSlower'])
+MOD_DUR = ORIG_DUR * TIMES_SLOWER
+FRAMES_WINDOW_SIZE = max(int(1 / MOD_DUR), 3)
+
+SEQ_THRESHOLD = int(args['threshold'])
 
 ####################################################################################################
 
@@ -48,14 +59,21 @@ def getDissonantIndexes(images, normal, framesDir):
     print("Hit SPACE for \'{}\'".format(normal.upper()))
     print("Press ENTER to continue")
     input()
+
     for classIndex, origIndex in tqdm(enumerate(images), total=len(images)):
         addFrame = False
 
         frame = cv2.imread(os.path.join(framesDir, "{}.jpg".format(origIndex)), cv2.IMREAD_COLOR)
+        if frame is None:
+            print("Error while openning {}.jpg".format(origIndex))
+            print("Exiting...")
+            exit()
+
         cv2.imshow("Frame", frame)
 
+
         timesPressedBefore = timesPressed
-        time.sleep(MOD_CUR)
+        time.sleep(MOD_DUR)
 
         if spacebarPressed:
             addFrame = True
@@ -101,28 +119,106 @@ def manualClassify(maybeWrong, framesDir):
     return certainBlink, certainNotBlink
 
 
+def checkForShortSequences(output):
+    seqSize = 0
+    seqStatus = output[0]
+
+    badSequences = []
+
+    i = 0
+    while i <= len(output):
+        if i < len(output) and output[i] == seqStatus:
+            seqSize += 1
+        else:
+            if seqSize <= SEQ_THRESHOLD:
+                seqList = list(range(i - seqSize, i))
+                if len(badSequences) >= 1 and badSequences[-1][-1] == seqList[0] - 1:
+                    badSequences[-1] += seqList
+                else:
+                    badSequences.append(seqList)
+            seqSize = 1
+
+            if i < len(output):
+                seqStatus = output[i]
+
+        i += 1
+
+    return badSequences
+
+
+def manualClassifyShortSeqs(shortSequences, framesDir):
+    certainBlink, certainNotBlink = [], []
+    for i, seq in enumerate(shortSequences):
+        print("Correcting sequence {}".format(i))
+
+        print("Reproducing sequence...")
+
+        playSequence = True
+        while playSequence:
+            for j in seq:
+                frame = cv2.imread(os.path.join(framesDir, "{}.jpg".format(j)), cv2.IMREAD_COLOR)
+                cv2.putText(frame, str(j), (int(frame.shape[1] * 0.5), int(frame.shape[0] * 0.7)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+                cv2.imshow("Frame", frame)
+                cv2.waitKey(50)
+
+            while True:
+                print("Play sequence again? Type Y for yes and N for no")
+                ans = input()
+                if ans == 'N':
+                    playSequence = False
+                    break
+                elif ans == 'Y':
+                    playSequence = True
+                    break
+                else:
+                    print("Wrong input")
+
+        certainBlinkSeq, certainNotBlinkSeq = manualClassify(seq, framesDir)
+        certainBlink += certainBlinkSeq
+        certainNotBlink += certainNotBlinkSeq
+
+    return certainBlink, certainNotBlink
+
+
+
+
 def main(classes, framesDir):
     output = [None for _ in range(len(classes))]
 
 
-    blinkImages = [i for i, x in enumerate(classes) if x == "blink"]
-    notBlinkImages = [i for i, x in enumerate(classes) if x == "not blink"]
+    blinkImages = [i for i, x in enumerate(classes) if x == "closed"]
+    notBlinkImages = [i for i, x in enumerate(classes) if x == "open"]
 
     for i in blinkImages: output[i] = True
     for i in notBlinkImages: output[i] = False
 
-    maybeWrongBlinkImages = getDissonantIndexes(blinkImages, "open", framesDir)
-
-    print("{} images selected".format(len(maybeWrongBlinkImages)))
-    print("Press ENTER to continue")
+    print("============================================================")
+    print("============================================================")
+    print("=============          FIRST PHASE           ===============")
+    print("============================================================")
+    print("============================================================")
+    print("============================================================")
+    print("Frame duration: " + str(MOD_DUR))
+    print("Frames window size: " + str(FRAMES_WINDOW_SIZE))
+    print("============================================================\n")
+    print("Press ENTER to continue:")
     input()
+
+    maybeWrongBlinkImages = getDissonantIndexes(blinkImages, "open", framesDir)
+    print("{} images selected".format(len(maybeWrongBlinkImages)))
+
 
     maybeWrongNotBlinkImages = getDissonantIndexes(notBlinkImages, "closed", framesDir)
     print("{} images selected".format(len(maybeWrongNotBlinkImages)))
 
     cv2.destroyAllWindows()
 
-    print("Press ENTER to continue")
+    print("============================================================")
+    print("============================================================")
+    print("============          SECOND PHASE           ===============")
+    print("============================================================")
+    print("============================================================")
+    print("Press ENTER to continue:")
     input()
 
     maybeWrong = maybeWrongBlinkImages + maybeWrongNotBlinkImages
@@ -132,6 +228,23 @@ def main(classes, framesDir):
 
     for i in certainBlink: output[i] = True
     for i in certainNotBlink: output[i] = False
+
+    print("============================================================")
+    print("============================================================")
+    print("=============          THIRD PHASE           ===============")
+    print("============================================================")
+    print("============================================================")
+    print("Press ENTER to continue:")
+    input()
+
+    shortSequences = checkForShortSequences(output)
+
+    certainBlink, certainNotBlink = manualClassifyShortSeqs(shortSequences, framesDir)
+
+    for i in certainBlink: output[i] = True
+    for i in certainNotBlink: output[i] = False
+
+    print("Exiting...")
 
     return output
 
@@ -145,7 +258,7 @@ def readCSV(inputPath):
         header = lines[0].split(';')
 
         frameStartIndex = header.index('frame_start')
-        isBlinkingIndex = header.index('eyes_status')
+        eyesStatusIndex = header.index('eyes_status')
 
         lines = lines[1:]
         numEntries = len(lines)
@@ -153,7 +266,7 @@ def readCSV(inputPath):
         for line in lines:
             line = line.split(';')
             frameStarts.append(line[frameStartIndex].strip())
-            originalClasses.append(line[isBlinkingIndex].strip())
+            originalClasses.append(line[eyesStatusIndex].strip())
 
     return numEntries, originalClasses, frameStarts
 
@@ -164,21 +277,13 @@ listener.start()
 
 ####################################################################################################
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-rp", "--inputCSV", required=True, help="Caminho para arquivo CSV de entrada")
-ap.add_argument("-fd", "--framesDir", required=True, help="Caminho para pasta onde ficarão salvas as imagens")
-ap.add_argument("-op", "--outputPath", required=True, help="Caminho para arquivo CSV de saída")
-args = vars(ap.parse_args())
-
-####################################################################################################
-
-numEntries, originalClasses, frameStarts = readCSV(args['inputCSV'])
+numEntries, originalClasses, frameStarts = readCSV(args['inputPath'])
 
 ####################################################################################################
 
 correctedClasses = main(originalClasses, args['framesDir'])
 with open(args['outputPath'], "w") as f:
-    f.write("index;frame_start;is_blinking;")
+    f.write("index;frame_start;eyes_status;")
     f.write('\n')
 
     for i in range(numEntries):
