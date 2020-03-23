b7c057e1211c91c088d46a56463eb17a8d6b989b
pauloamed <pauloaugusto99@ufrn.edu.br>
2020-03-23 11:12:42 -0300

    folder for others scripts

---
+++ others/rename_annotated_files.py
@@ -0,0 +1,34 @@
+import os
+
+def renameFiles(pathsList, name):
+    for i, x in enumerate(pathsList):
+        y = os.path.join(datasetPath, name + "_{}.jpg".format(i))
+        os.rename(x, y)
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-dp", "--datasetPath", required=True, help="")
+args = ap.parse_args()
+
+datasetPath = args.datasetPath
+
+openEyesFiles = []
+closedEyesFiles = []
+
+for x in os.listdir(datasetPath):
+    y = x.split('_')
+
+    newName = os.path.join(datasetPath, "~" + x)
+    x = os.path.join(datasetPath, x)
+    os.rename(x, newName)
+
+    if 'closed' in y:
+        closedEyesFiles.append(newName)
+    else:
+        openEyesFiles.append(newName)
+
+
+renameFiles(openEyesFiles, "open")
+renameFiles(closedEyesFiles, "closed")
+
+
+print("Exiting...")
