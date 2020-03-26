ae0e8a3c86b7cacc53d3959aa5c234a43adf568e
pauloamed <pauloaugusto99@ufrn.edu.br>
2020-03-26 16:02:19 -0300

    updated script for using blink_utils repo

--- annotators/pre_annotator.py
+++ annotators/pre_annotator.py
@@ -2,120 +2,29 @@
 
 import argparse
 import pickle
-import dlib
 import cv2
 import os
 import numpy as np
 from tqdm import tqdm
 
-import keras
-import tensorflow as tf
-from keras import backend as K
-from keras.models import load_model
-
 DATA_PATH = "modelFiles/"
-LEFT_EYE_INDEXES = range(36, 42)
-RIGHT_EYE_INDEXES = range(42, 48)
 
-##########################################################################################
+from blink_utils.models.face_detectors import *
+from blink_utils.models.classifiers import *
+from blink_utils.models.roi_extractors import *
 
-import cv2
-from math import hypot
-
-
-modelFile = os.path.join(DATA_PATH, "opencv_face_detector_uint8.pb")
-configFile = os.path.join(DATA_PATH, "opencv_face_detector.pbtxt")
-
-cnnNet = cv2.dnn.readNetFromTensorflow(modelFile, configFile)
-cnnThreshold = 0.9
-
-def facePunct(face, frameCenter):
-    area = abs(face['x2'] - face['x1']) * abs(face['y2'] - face['y1'])
-    faceCenter = ((face['x1'] + face['x2']) // 2, (face['y1'] + face['y2']) // 2)
-    distCenters = hypot(frameCenter[0] - faceCenter[0], frameCenter[1] - faceCenter[1])
-    confidence = face['conf']
-    return area * 1/distCenters * confidence
-
-
-def getFace(cnnNet, frame):
-    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), [104, 117, 123], False, False)
-    cnnNet.setInput(blob) # blob sera a entrada da rede
-    detections = cnnNet.forward() # deteccoes feitas na rede
-
-    aboveThresholdFaces = []
-
-    for i in range(detections.shape[2]):
-        confidence = detections[0, 0, i, 2]
-        if confidence > cnnThreshold:
-            face = {
-                'conf' : confidence,
-                'x1' : max(int(detections[0, 0, i, 3] * frame.shape[1]), 0),
-                'y1' : max(int(detections[0, 0, i, 4] * frame.shape[0]), 0),
-                'x2' : int(detections[0, 0, i, 5] * frame.shape[1]),
-                'y2' : int(detections[0, 0, i, 6] * frame.shape[0]),
-            }
-            aboveThresholdFaces.append(face)
-
-    frameCenter = (frame.shape[0]//2, frame.shape[1]//2)
-
-    bestFace = max(aboveThresholdFaces, key=lambda x : facePunct(x, frameCenter))
-
-    return bestFace
-
-######################################## AUX #############################################
-
-def getEyeImage(gray, eyeIndexes, landmarks):
-    eyePoints = [(landmarks.part(i).x, landmarks.part(i).y) for i in eyeIndexes]
-
-    (x, y, w, h) = cv2.boundingRect(np.array([eyePoints]))
-    return gray[y:y + h, x:x + w]
-
-##################################### CNN MODEL ##########################################
-num_cores = 4
-
-num_CPU = 1
-num_GPU = 0
-
-config = tf.ConfigProto(intra_op_parallelism_threads=num_cores,
-                        inter_op_parallelism_threads=num_cores,
-                        allow_soft_placement=True,
-                        device_count = {'CPU' : num_CPU,
-                                        'GPU' : num_GPU})
-
-session = tf.Session(config=config)
-K.set_session(session)
-
-model = load_model(os.path.join(DATA_PATH, "weights.149-0.01.hdf5"))
-
-def predict_eye_state(model, image):
-    image = cv2.resize(image, (20, 10))
-    image = image.astype(dtype=np.float32)
-
-    image_batch = np.reshape(image, (1, 10, 20, 1))
-    image_batch = keras.applications.mobilenet.preprocess_input(image_batch)
-
-    output = model.predict(image_batch)[0]
-    print(output)
-    return np.argmax(output)
-
-
-def checkIfBlinking(model, leftEye, rightEye):
-    leftEyePredict = predict_eye_state(model, leftEye)
-    rightEyePredict = predict_eye_state(model, rightEye)
-
-    return (leftEyePredict == 0) or (rightEyePredict == 0)
-
-######################################## ARGS ##########################################
 
 ap = argparse.ArgumentParser()
+ap.add_argument("-fp", "--filesPath", required=True, help="")
 ap.add_argument("-rp", "--recordsPath", required=True, help="Caminho para arquivo onde ficarão salvas as informacoes para cada frame")
 ap.add_argument("-fd", "--framesDir", required=True, help="Caminho para pasta onde ficarão salvas as imagens")
 ap.add_argument("-op", "--outputPath", required=True, help="Caminho para arquivo de saída")
 args = vars(ap.parse_args())
 
-############################## LANDMARKS PREDICTOR PREP ################################
+faceDetector = CNNOpenCV(args['filesPath'])
+roiExtractor = ROI68LandmarksDlib(args['filesPath'], pointsList = ['left', 'right'], scales = [[(1, 1)], [(1, 1)]], updateRound = 1)
+classifier = KristenCNNTensorFlow(args['filesPath'])
 
-predictor = dlib.shape_predictor(os.path.join(DATA_PATH, "shape_predictor_68_face_landmarks.dat"))
 
 ################################# PICKLE/INPUT LOGIC ###################################
 
@@ -136,27 +45,26 @@ with open(args['outputPath'], "w") as f:
 
         ## RETRIEVING FACE FRAME AND LANDMARKS
         frame = cv2.imread(os.path.join(args['framesDir'], "{}.jpg".format(i)), cv2.IMREAD_COLOR)
-        if frame is None:
-            raise Exception("Não foi possível abrir frame {}".format(i))
 
-        facePuncts = getFace(cnnNet, frame)
-        faceFrame = frame[facePuncts['y1']:facePuncts['y2'], facePuncts['x1']:facePuncts['x2']]
-        gray = cv2.cvtColor(faceFrame, cv2.COLOR_BGR2GRAY)
+        facePoints = faceDetector(frame)
+        if facePoints == None:
+            print("Cant find face")
+            continue
+        faceFrame = frame[facePoints['y1']:facePoints['y2'], facePoints['x1']:facePoints['x2']]
+        frame = faceFrame.copy()
 
-        landmarks = predictor(faceFrame, dlib.rectangle(0, 0, faceFrame.shape[1], faceFrame.shape[0]))
-
-        leftEye = getEyeImage(gray, LEFT_EYE_INDEXES, landmarks)
-        rightEye = getEyeImage(gray, RIGHT_EYE_INDEXES, landmarks)
-
-        cv2.imshow("le", leftEye)
-
-        isBlinking = checkIfBlinking(model, leftEye, rightEye)
+        eyesPointsFromScales = roiExtractor(frame)
+        eyesFrames = [frame[eyesPoints['y1']:eyesPoints['y2'], eyesPoints['x1']:eyesPoints['x2']]
+                        for eyesPoints in eyesPointsFromScales]
+        frame = eyesFrames[0].copy()
+        output = classifier(eyesFrames[0], eyesFrames[1])
 
         ## RETRIEVING VALUES TO BE WRITTEN ON CSV FILE
         frameStart = record
-        values = [str(i), "%.3f" % frameStart, "closed" if isBlinking else "open"]
+        values = [str(i), "%.3f" % frameStart, "closed" if output else "open"]
 
-        cv2.imshow("Face", gray)
+        cv2.imshow("Face", faceFrame)
+        cv2.imshow("ROI", eyesFrames[0])
         cv2.waitKey(1)
 
         ## WRITING
