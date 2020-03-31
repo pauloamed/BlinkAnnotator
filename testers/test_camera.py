61fc620e02f058774ed8d96bc73f7f3cf3718cc6
pauloamed <pauloaugusto99@ufrn.edu.br>
2020-03-31 10:53:00 -0300

    updt test_carema

--- testers/test_camera.py
+++ testers/test_camera.py
@@ -4,13 +4,24 @@ import time
 import random
 import cv2
 
+import numpy as np
+
+import imutils
+
+import math
+
 import argparse
 
-from utils import loadModels
 
 from blink_utils.models.face_detectors import *
 from blink_utils.models.classifiers import *
 from blink_utils.models.roi_extractors import *
+
+from blink_utils.models.face_aligners import *
+
+from blink_utils import BlinkLengthEstimator
+
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-fp", "--filesPath", required = True)
@@ -18,12 +29,16 @@ args = parser.parse_args()
 
 filesPath = args.filesPath
 
-faceDetector = CNNOpenCV(args.filesPath)
-roiExtractor = ROI68LandmarksDlib(args.filesPath, pointsList = ['sboth'], scales = [[(3, 3)]], updateRound = 1)
-classifier = HaarOpenCV(args.filesPath)
+faceDetector = UltraLight(args.filesPath, threshold = 0.8)
 
 aligner = Aligner5LandmarksDlib(args.filesPath)
 
+roiExtractor = ROI5LandmarksDlib(args.filesPath, pointsList = ['left', 'right'], scales = [[(1.1, 1)], [(1.1, 1)]], updateRound = 1)
+classifier = SigmoidCNNPytorch(args.filesPath, threshold = 0.1, modelFile = "olho_20x20.pt", fixedSize = (20, 20))
+# classifier = KristenCNNTensorFlow(args.filesPath)
+# classifier = HaarOpenCV(args.filesPath)
+#
+lengthEstimator = BlinkLengthEstimator(window = 11, threshold = 0.5, log=False)
 
 videoCapture = None
 try:
@@ -35,12 +50,21 @@ except:
 
 showTime = False
 
+frameCount = 0
+lastSec = math.floor(time.time())
+
+total = 0
+timeFace = 0
+timeRoi = 0
+timeClass = 0
 while True:
-    loopStart = time.time()
+    total += 1
     _, frame = videoCapture.read()
+    cv2.imshow("a", frame)
 
     startFaceDetector = time.time()
     facePoints = faceDetector(frame)
+    timeFace += time.time() - startFaceDetector
     if showTime: print("Face Detector: {:.4f}".format(time.time() - startFaceDetector))
     if facePoints == None:
         print("Cant find face")
@@ -48,26 +72,47 @@ while True:
     faceFrame = frame[facePoints['y1']:facePoints['y2'], facePoints['x1']:facePoints['x2']]
     frame = faceFrame
 
+    angle = aligner(frame)
+
+    frame = imutils.rotate_bound(frame, angle * 180 / np.pi)
+
+    cv2.imshow("b", frame)
+
+    eyesFrames = None
     if roiExtractor:
         startRoiExtractor = time.time()
         eyesPointsFromScales = roiExtractor(frame)
+        timeRoi += time.time() - startRoiExtractor
         if showTime: print("Roi Extractor: {:.4f}".format(time.time() - startRoiExtractor))
         eyesFrames = [frame[eyesPoints['y1']:eyesPoints['y2'], eyesPoints['x1']:eyesPoints['x2']]
                         for eyesPoints in eyesPointsFromScales]
         frame = eyesFrames[0]
 
-    cv2.imshow("a", frame)
+    cv2.imshow("c", frame)
 
     startClassifier = time.time()
-    output = classifier(frame)
+    output = classifier(eyesFrames[0])
+    print(output)
+
+    lengthEstimator.addOutput(output)
+
+    print(lengthEstimator.getLastBlink())
+
+    timeClass += time.time() - startClassifier
     if showTime: print("Classifer: {:.4f}".format(time.time() - startClassifier))
 
 
-    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" if output else "")
-    if showTime:
-        fps = 1 / (time.time() - loopStart)
-        print("FPS: {}".format(fps), time.time() - loopStart)
+
+    # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" if output else "")
+
+    frameCount += 1
+    if math.floor(time.time()) > lastSec:
+        print("FPS: {}".format(frameCount))
+        lastSec = math.floor(time.time())
+        frameCount = 0
 
     # logica do opencv, aperta q pra fechar
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        continue
+        break
+
+print(timeFace / total, timeRoi / total, timeClass / total)
